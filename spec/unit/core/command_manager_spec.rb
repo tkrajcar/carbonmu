d2eeccfa44b314d30ab0abcaa58b8cb9267c09e7
Tim Krajcar <allegro@conmolto.org>
2014-03-18 20:02:16 -0700

    add contexts to commandmanagerspec and use 'subject'

--- spec/unit/core/command_manager_spec.rb
+++ spec/unit/core/command_manager_spec.rb
@@ -1,27 +1,29 @@
 require 'spec_helper'
 
 describe CommandManager do
-  it 'registers and stores the proper data' do
-    CommandManager.add :test_command_manager do
-      "Foo"
-    end
-    CommandManager.commands[:test_command_manager].should be_a(Proc)
-  end
-
   it 'prevents instantiation' do
-    expect { CommandManager.new }.to raise_error(NoMethodError)
+    expect { subject.new }.to raise_error(NoMethodError)
   end
 
-  it "handles a bad command" do
-    Notify.should_receive(:all) #TODO
-    context = double("CommandContext")
-    subject.execute("DEFINITELY_NEVER_GOING_TO_BE_A_GOOD_COMMAND", context)
+  context '#add' do
+    it 'registers and stores a Proc' do
+      subject.add :test_command_manager do; "Foo"; end
+      subject.commands[:test_command_manager].should be_a(Proc)
+    end
   end
 
-  it "dispatches a good command" do
-    load_rel 'fixtures/test_parser_command.rb'
-    subject.commands[:testing_parser_command].should_receive(:call)
-    context = double("CommandContext")
-    subject.execute("testing_parser_command", context)
+  context '#execute' do
+    it "handles a bad command" do
+      Notify.should_receive(:all) #TODO
+      context = double("CommandContext")
+      subject.execute("DEFINITELY_NEVER_GOING_TO_BE_A_GOOD_COMMAND", context)
+    end
+
+    it "dispatches a good command" do
+      load_rel 'fixtures/test_parser_command.rb'
+      subject.commands[:testing_parser_command].should_receive(:call)
+      context = double("CommandContext")
+      subject.execute("testing_parser_command", context)
+    end
   end
 end
