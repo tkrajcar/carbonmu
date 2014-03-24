56c8ca132599e2bb722cb0605c65e86d4fd1a031
Tim Krajcar <allegro@conmolto.org>
2014-03-24 13:55:30 -0700

    Update FinalizedConnection to new CommandContext format

--- lib/overlord/connection/finalized_connection.rb
+++ lib/overlord/connection/finalized_connection.rb
@@ -4,8 +4,8 @@ module CarbonMU
 
     class FinalizedConnection < ConnectionState
       def handle_input(input)
-        command_context = CommandContext.new(@conn)
-        Parser.parse(input, command_context)
+        command_context = CommandContext.new(enacting_connection_id: @conn.id, command: input)
+        Parser.parse(command_context)
       end
     end
 
