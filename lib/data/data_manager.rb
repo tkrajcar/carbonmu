8a9364425aa34e6bf9476a9c80e9f4f74cf86208
Tim Krajcar <allegro@conmolto.org>
2014-03-27 07:56:12 -0700

    proper variable scope

--- lib/data/data_manager.rb
+++ lib/data/data_manager.rb
@@ -2,18 +2,18 @@ require 'celluloid'
 
 module CarbonMU
   module DataManager
-    @@store = {}
+    @store = {}
 
     def self.persist(obj)
       data_engine.async.persist(obj.dup)
     end
 
     def self.[]=(key, value)
-      @@store[key] = value
+      @store[key] = value
     end
 
     def self.[](key)
-      @@store[key] || raise(ObjectNotFoundError, "Requested object '#{key}' not present in DataManager!")
+      @store[key] || raise(ObjectNotFoundError, "Requested object '#{key}' not present in DataManager!")
     end
 
     def self.data_engine
