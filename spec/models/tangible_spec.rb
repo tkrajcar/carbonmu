1e2c253827b229b6995e66f9c18cf5708b34eb7d
Tim Krajcar <allegro@conmolto.org>
2015-02-27 23:15:09 -0800

    relocate expectation into proper place

--- spec/models/tangible_spec.rb
+++ spec/models/tangible_spec.rb
@@ -2,6 +2,4 @@ require 'spec_helper'
 
 describe Tangible do
   it_behaves_like "a Tangible implementation"
-
-  it { is_expected.to have_many(:contents).of_type(Movable).with_foreign_key(:location) }
 end
