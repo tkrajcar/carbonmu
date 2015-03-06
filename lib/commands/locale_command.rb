bb5948a4194ad4189add62e1062f41af1417b4fb
Tim Krajcar <allegro@conmolto.org>
2015-03-05 18:06:56 -0800

    Add temp locale-changing command.

---
+++ lib/commands/locale_command.rb
@@ -0,0 +1,12 @@
+# TODO TEMP
+module CarbonMU
+  class LocaleCommand < Command
+    syntax /^locale (?<locale>.*)/
+    syntax /^do (?<text>.*)/
+
+    def execute
+      @context.enacting_connection.locale = @params[:locale]
+      Notify.one(@context.enacting_connection, "Locale changed to #{@params[:locale]}.")
+    end
+  end
+end
