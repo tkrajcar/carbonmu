8f60692f34da1ad83e5f39d62de8c35aac257308
Tim Krajcar <allegro@conmolto.org>
2015-03-25 09:37:00 -0700

    Fix parser spec to not explode

--- spec/lib/core/parser_spec.rb
+++ spec/lib/core/parser_spec.rb
@@ -5,16 +5,18 @@ describe Parser do
     syntax "testing_good_command"
   end
 
+  let (:connection) { double(Connection).as_null_object }
+
   context '.parse_and_execute' do
     it "handles a bad command" do
       expect_any_instance_of(UnknownCommand).to receive(:execute)
 
-      subject.parse_and_execute(1, "DEFINITELY_NEVER_GOING_TO_BE_A_GOOD_COMMAND")
+      subject.parse_and_execute(connection, "DEFINITELY_NEVER_GOING_TO_BE_A_GOOD_COMMAND")
     end
 
     it "calls .execute on a good command" do
       expect_any_instance_of(ParseTestCommand).to receive(:execute)
-      subject.parse_and_execute(1, "testing_good_command")
+      subject.parse_and_execute(connection, "testing_good_command")
     end
   end
 
